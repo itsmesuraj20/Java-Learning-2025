d8ba3a83bc11f564d78d53f2ac0f227f9739ec14
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2025-01-03 23:35:08 +0530

    Scamer Class Practice

--- Basic-Programming/ScannerClass.java
+++ Basic-Programming/ScannerClass.java
@@ -3,6 +3,11 @@ public class ScannerClass{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int n , a , b,c ;
+        int n , a , b, sum = 0 ;
+        for (int i = 0; i < n; i++) {
+            sum = sum + (Math.max(a, b));
+        }
+
+        System.out.println(sum);
     }
 }
