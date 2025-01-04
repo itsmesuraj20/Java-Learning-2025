1f6bb8f14373d26dacf437be1415cccca3bde20b
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2025-01-04 23:44:40 +0530

    Day 2

---
+++ Core-Programming-Fundamentals/ScannerClass2.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class ScannerClass2{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner()
+        
+        int arr[5] = {1,2,3,4,5};
+        int n = sc.nextInt();
+        for (int i = 0; i < 5; i++) {
+            
+        }
+    }
+}
