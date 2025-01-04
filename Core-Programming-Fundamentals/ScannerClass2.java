0aae2a6e75f19cb7e6d926e81d6c9d00d2b7d3dc
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2025-01-04 23:45:31 +0530

    Some fixs

--- Core-Programming-Fundamentals/ScannerClass2.java
+++ Core-Programming-Fundamentals/ScannerClass2.java
@@ -1,11 +1,11 @@
 import java.util.*;
 public class ScannerClass2{
     public static void main(String[] args) {
-        Scanner sc = new Scanner()
-        
-        int arr[5] = {1,2,3,4,5};
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+        int arr[] = {1,2,3,4,5};
         int n = sc.nextInt();
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < n; i++) {
             
         }
     }
