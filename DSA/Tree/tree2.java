7907d4fb6a09cd9c90e44fb1b8e1609fb8f49388
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2025-01-16 23:59:57 +0530

    Tree

--- DSA/Tree/tree2.java
+++ DSA/Tree/tree2.java
@@ -3,7 +3,7 @@ package DSA.Tree;
 public class tree2 {
     public static void main(String args[])
     {
-        System.out.println("Tree 2");
-        
+       System.out.println("Tree 2");
+
     }
 }
