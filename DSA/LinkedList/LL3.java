0e52655d2da4f5df8b1ea90d51672984ea2d2147
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2025-01-19 23:11:20 +0530

    Test

--- DSA/LinkedList/LL3.java
+++ DSA/LinkedList/LL3.java
@@ -1,7 +1,7 @@
 package DSA.LinkedList;
 
 public class LL3 {
-    public static void main (Stirng args[]){
+    public static void main (String args[]){
         System.out.println("LL3");
     }
 }
