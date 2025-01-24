3dfd6030260a4f159cf033199b45e55650ea3056
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2025-01-24 23:41:46 +0530

    Array

--- DSA/Array/array1.java
+++ DSA/Array/array1.java
@@ -4,6 +4,7 @@ public class array1 {
     public static void main(String[] args) {
         System.out.println("Array");
 
-        int arr[5] = {1,2,3,4,5};
+        int arr[] = {1,2,3,4,5};
+
     }
 }
