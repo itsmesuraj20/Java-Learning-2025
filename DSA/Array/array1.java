3690814e998fb7e2627b6bb54afed49d9b725265
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2025-01-24 23:48:14 +0530

    Array

--- DSA/Array/array1.java
+++ DSA/Array/array1.java
@@ -4,9 +4,9 @@ public class array1 {
     public static void main(String[] args) {
         System.out.println("Array");
 
-        int arr[] = {1,2,3,4,5};
+        int arr[] = {1, 2, 3, 4, 5};
         for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i] + "Hello World" + i);
+            System.out.println("Element: " + arr[i] + ", Index: " + i);
         }
 
     }
