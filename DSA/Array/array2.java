ccceaca6d52a0ce1853eb9e2d062d2b6adf42036
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2025-01-25 22:29:58 +0530

    Push

--- DSA/Array/array2.java
+++ DSA/Array/array2.java
@@ -1,7 +1,18 @@
 package DSA.Array;
 
+import java.util.Arrays;
+
 public class array2 {
     public static void main(String[] args) {
-        System.out.println("Array");
+        int[] arr = { 5, 2, 8, 1, 3 };
+
+        // Sorting the array
+        Arrays.sort(arr);
+
+        // Printing the sorted array
+        System.out.println("Sorted Array:");
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
     }
 }
