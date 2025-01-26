186ebe1fce3a1a037b4c6558a1d91db8d228d55b
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2025-01-26 16:34:49 +0530

    MultiThreading

--- MultiThreading/multithreading.java
+++ MultiThreading/multithreading.java
@@ -1,5 +1,23 @@
 package MultiThreading;
+class MyThread extends Thread {
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(Thread.currentThread().getId() + " Value: " + i);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                System.out.println(e);
+            }
+        }
+    }
+}
 
 public class multithreading {
-    
+    public static void main(String[] args) {
+        MyThread t1 = new MyThread();
+        MyThread t2 = new MyThread();
+        
+        t1.start();
+        t2.start();
+    }
 }
