6f3f4169addbf0c65a025f9d3cf231edb250e8db
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2025-02-02 20:46:42 +0530

    fix

--- DSA/Searching and Sorting/linearsearch.java
+++ DSA/Searching and Sorting/linearsearch.java
@@ -2,6 +2,7 @@ package DSA.Searching and Sorting;
 
 public class linearsearch {
     public static void main(String[] args) {
+        
         System.out.println("Hello World from linearsearch.java");
     }
 }
