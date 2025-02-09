28125609bef45e5b58bafaadf9790a5957eba919
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2025-02-09 23:16:12 +0530

    Some More Tweaks

--- DSA/Array/array1.java
+++ DSA/Array/array1.java
@@ -4,7 +4,6 @@ public class array1 {
     public static void main(String[] args) {
         System.out.println("Array Elements:");
 
-        
         int[] arr = { 1, 2, 3, 4, 5 };
         for (int i = 0; i < arr.length; i++) {
             System.out.println("Element: " + arr[i] + ", Index: " + i);
